560141719ddf0bac70eb246276848bf9a280a495
Vo Van Hau <hauvo1709@gmail.com>
2022-09-18 15:37:34 +0700

    CRUD blog/posts

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Posts(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 255)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs = {'pk': self.pk})
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,6 +1,8 @@
 from django.template import loader
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Posts
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
 
 def home(request):
     template = loader.get_template('home.html')
@@ -16,3 +18,45 @@ def about(request):
         'title': 'Blog About'
     }
     return HttpResponse(template.render(context, request))
+
+class PostListView(ListView):
+    model = Posts
+    template_name = 'home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Posts
+    template_name = 'posts/detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Posts
+    template_name = 'posts/upsert.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Posts
+    template_name = 'posts/upsert.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Posts
+    template_name = 'posts/delete.html'
+    success_url = '/blog/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
